cf8acf3b026b821eb2770143dadb8d71a64de998
Ayman29996266 <ayman.29996266.ran@gmail.com>
2025-01-20 18:43:35 +0530

    Added basic error handling and comments.

--- src/client.c
+++ src/client.c
@@ -1,34 +1,79 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+/* Libraries needed for the client. */
+
 
 
 int main(int argc, char *argv[]) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    /* Create a socket */
+    int client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sockfd == -1) {                                  // error handling, in case the socket creation failed
+        perror("Error... Couldn't initialize a socket.\n");
+        exit(1);
+    }
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port   = htons(9000);
 
-    inet_pton(AF_INET, argv[1], &address.sin_addr);
 
-    connect(sockfd, (struct sockaddr*)&address, sizeof(address));
+    /* A structure to hold the server address and configuration */
+    char *IP_address = argv[1]? argv[1] : "127.0.0.1";          // IP address can be passed as program argument or defaults to local host
+    struct sockaddr_in server_address;
+    server_address.sin_family       = AF_INET;                  // Set the protocol     (IP)
+    server_address.sin_port         = htons(9000);              // Set the port         (9000)
+    server_address.sin_addr.s_addr  = inet_addr(IP_address);    // Set the IP address   (Local host)
 
-    char buffer[2048] = {0};
-    char message[2048] = {0};
+
+
+    /* Connect the socket to the address */
+    int connection_result = connect(client_sockfd, (struct sockaddr*)&server_address, sizeof(server_address));
+    
+    if (connection_result == -1) {                              // error handling, in case connection failed
+        perror("Error... Couldn't connect to the server.\n");
+        close(client_sockfd);
+        exit(1);
+    }
+
+
+
+    /* Initiate the communication cycle */
+    char buffer[2048]   = {0};                                  // buffer to store incoming messages
+    char message[2048]  = {0};
+    int  message_length = strlen(message);
     while (1) {
         printf("Enter your message: ");
         scanf("%s", message);
-        send(sockfd, message, strlen(message), 0);
-        read(sockfd, buffer, 2048 -1);
+        send(client_sockfd, message, message_length, 0);
 
-        if (!strcmp(message, "exit") || !strcmp(message, "bye")) {
+        int bytes_received      = recv(client_sockfd, buffer, 2048 -1, MSG_DONTWAIT);
+        if (bytes_received < 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {                
+            } else {
+                perror("Error... something wrong happened with 'recv'.");
+                close(client_sockfd);
+                exit(1);
+            }
+        } else if (bytes_received > 0) {
+            buffer[bytes_received]  = '\0';
+            printf("\nServer > %s\n", buffer);
+            // buffer[0] = '\0';
+        } else {
+            break;
+        }
+
+        // handle session closing
+        if (!strcmp(message, "exit") || !strcmp(message, "bye") || !strcmp(message, "close")) {
             break;
         }
     }
 
-    close(sockfd);
+
+    /* Terminate the connection */
+    close(client_sockfd);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,45 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+/* Libraries needed for the server. */
+
 
 
 int main(int argc, char *argv[]) {
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    /* Create a socket */
+    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sockfd == -1) {                                  // error handling
+        perror("Error... Couldn't initialize a socket.\n");
+        exit(1);
+    }
 
-    struct sockaddr_in address;
-    address.sin_family      = AF_INET;
-    address.sin_port        = htons(9000);
-    address.sin_addr.s_addr = INADDR_ANY;
 
-    socklen_t socketLength = (socklen_t)sizeof(address);
 
-    bind(sockfd, (struct sockaddr*)&address, socketLength);
-    listen(sockfd, 1);
+    /* A structure to hold the server address and configuration */
+    struct sockaddr_in server_address;
+    server_address.sin_family       = AF_INET;                   // Set the protocol     (IP)
+    server_address.sin_port         = htons(9000);               // Set the port         (9000)
+    server_address.sin_addr.s_addr  = htonl(INADDR_ANY);         // Set the IP address   (Local host)
+    socklen_t server_socklen        = sizeof(server_address);
 
-    int client = accept(sockfd, (struct sockaddr*)&address, &socketLength);
 
-    char succcessMessage[] = "connection established successfully.";
+
+    /* Bind the socket to the address*/
+    int binding_result = bind(server_sockfd, (struct sockaddr*)&server_address, server_socklen);
+    if (binding_result == -1) {                                 // error handling
+        perror("Error... Couldn't bind the socket.\n");
+        close(server_sockfd);
+        exit(1);
+    }
+
+
+
+    /* Set the limits of the server */
+    int listening_result = listen(server_sockfd, 1);
+    if (listening_result == -1) {                               // error handling
+        perror("Error... An issue occured while listening to the port.\n");
+        close(server_sockfd);
+        exit(1);
+    }
+
+
+
+    /* Accept the client connection request and send a greeting message */
+    struct sockaddr_in client_address;
+    socklen_t client_socklen = sizeof(client_address);
+    int  client = accept(server_sockfd, (struct sockaddr*)&client_address, &client_socklen);
+
+    if (client == -1) {                                         // error handling
+        perror("Error... Couldn't connect to client.\n");
+        close(server_sockfd);
+        exit(1);
+    }
+
+    char succcessMessage[]  = "connection established successfully.\n";
     send(client, succcessMessage, strlen(succcessMessage), 0);
 
-    char buffer[2048] = {0};
-    char message[] = "message was recieved.";
 
+
+    /* Initiate the communication cycle */
+    char buffer[2048] = {0};                                    // buffer to store incoming messages
     while (1) {
-        read(client, buffer, 2048 - 1);
-        buffer[strlen(buffer)] = '\0';
+        int bytes_received = recv(client, buffer, 2048 - 1, 0);
+        if (bytes_received > 0) {                               // ignore empty messages
+            buffer[bytes_received] = '\0';                      // NULL terminate to prevent buffer overflow
+            printf("Client > %s\n", buffer);
 
-        send(client, message, strlen(message), 0);
-
-        if (!strcmp(buffer, "exit") || !strcmp(buffer, "bye")) {
+        } else if (bytes_received == 0) {                       // When client terminates the connection    
             break;
+        } else {                                                // error handling
+            perror("Error in the 'recv' function.\n");
+            close(client);
+            close(server_sockfd);
+            exit(1);
         }
     }
 
-    close(sockfd);
+
+    /* Terminate the connection */
     close(client);
+    close(server_sockfd);
+
     return 0;
 }
